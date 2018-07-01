4aad2ee89c9cc9b377f9efc7eeda80c5582ad704
josephskrzysowski <josephskrzysowski@gmail.com>
2018-07-01 12:27:45 -0400

    too big for thuinderbird

--- DjangoApp/Baker_Tracker/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/arcarius/DevWork/Python_Tools/SON_Analyst_Programmer_Technology_Test/DjangoApp/Baker_Tracker/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
